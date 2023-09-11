7d4a651a7cab0a346c6e92efdc4c74b668e0af1a
vherawidatama <vherawidatama@gmail.com>
2023-09-11 17:55:18 +0000

    Change git am to git apply, cause format not support

--- patch/patch.sh
+++ patch/patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 PATCH_LOC=$PWD/device/xiaomi/marble/patch
 cd frameworks/native
-git am $PATCH_LOC/frameworks/native/Vibration_OSS_marble.patch
+git apply $PATCH_LOC/frameworks/native/Vibration_OSS_marble.patch
 cd libs/gui
 git am $PATCH_LOC/frameworks/native/libs/gui/0001-Add-Support-FEAS.patch
